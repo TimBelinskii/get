614adcfeee9dc425a4ff6c654b437131c87b94f2
Тимофей Белинский <belinskii.td@phystech.edu>
2023-04-04 17:22:20 +0300

    minor changes

--- 5.АЦП/5-1-adc-simple.py
+++ 5.АЦП/5-1-adc-simple.py
@@ -1,4 +1,7 @@
+import time
+
 import RPi.GPIO as GPIO
+import time
 
 
 def decimal2binary(value):
@@ -6,7 +9,14 @@ def decimal2binary(value):
 
 
 def adc():
-    pass
+    for v in range(256):
+        signal = decimal2binary(v)
+        GPIO.output(dac, signal)
+        voltage = v / 256 * 3.3
+        time.sleep(0.0007)
+        comp_v = GPIO.input(comp)
+        if comp_v == 0:
+            print(f'Значение АЦП: {v} -> {signal}, поданное напряжение: {round(voltage, 2)}')
 
 
 dac = [26, 19, 13, 6, 5, 11, 9, 10]
@@ -15,7 +25,7 @@ troyka = 17
 
 GPIO.setmode(BCM)
 GPIO.setup(dac, GPIO.OUT)
-GPIO.setup(troyka, GPIO.OUT, initial=1)
+GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(comp, GPIO.IN)
 
 try:
