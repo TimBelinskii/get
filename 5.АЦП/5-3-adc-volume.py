e639b1f846ed31f8d7f615aa0f0c385c989d7f6d
Тимофей Белинский <belinskii.td@phystech.edu>
2023-04-04 18:22:39 +0300

    first v

--- 5.АЦП/5-3-adc-volume.py
+++ 5.АЦП/5-3-adc-volume.py
@@ -1,7 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def decimal2binary(value):
+    return [int(bit) for bit in bin(value)[2:].zfill(8)]
+
+
+def adc():
+    signal = [0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(8):
+        signal[i] = 1
+        GPIO.output(dac, signal)
+        time.sleep(0.0007)
+        comp_v = GPIO.input(comp)
+        signal[i] = comp_v
+    v = 0
+    for i in range(7):
+        v += signal[i] * 2 ** (7 - i)
+    return v, signal
+
+
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+leds = [21, 20, 16, 12, 7, 8, 25, 24]
+comp = 4
+troyka = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac + leds, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(comp, GPIO.IN)
+
 try:
     while True:
-        adc()
+        value, signal = adc()
+        GPIO.output(leds, signal)
+        voltage = round(value / 256 * 3.3, 2)
+        print(f"Цифровое значение: {value} -> {signal}, значение напряжения: {voltage}")
 
 finally:
-    GPIO.output(dac + [comp], 0)
+    GPIO.output(dac + [troyka] + leds, GPIO.LOW)
     GPIO.cleanup()
