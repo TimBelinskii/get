72d4bbb2d8c9ea2c0a2b2133fc014d51d48f070e
Тимофей Белинский <belinskii.td@phystech.edu>
2023-04-18 19:16:18 +0300

    work check

---
+++ 7.Автоматизированные измерения/7.auto_measuring.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def decimal2binary(value):
+    return [int(bit) for bit in bin(value)[2:].zfill(8)]
+
+
+def adc():
+    signal = [0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(8):
+        signal[i] = 1
+        GPIO.output(dac, signal)
+        time.sleep(0.0007)
+        comp_v = GPIO.input(comp)
+        signal[i] = comp_v
+    v = 0
+    for i in range(7):
+        v += signal[i] * 2 ** (7 - i)
+    return v, signal
+
+
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+comp = 4
+troyka = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(comp, GPIO.IN)
+
+try:
+    while True:
+        value, signal = adc()
+        voltage = round(value / 256 * 3.3, 2)
+        print(f"Цифровое значение: {value} -> {signal}, значение напряжения: {voltage}")
+finally:
+    GPIO.output(dac + [troyka], GPIO.LOW)
+    GPIO.cleanup()
