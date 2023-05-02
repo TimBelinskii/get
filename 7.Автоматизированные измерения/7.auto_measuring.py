27800bb6263172f2c7549a89a1497190aa65c110
Тимофей Белинский <belinskii.td@phystech.edu>
2023-05-02 17:17:37 +0300

    start of 7th task

--- 7.Автоматизированные измерения/7.auto_measuring.py
+++ 7.Автоматизированные измерения/7.auto_measuring.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import matplotlib.pyplot as plt
 import time
 
 
@@ -21,19 +22,39 @@ def adc():
 
 
 dac = [26, 19, 13, 6, 5, 11, 9, 10]
+leds = [21, 20, 16, 12, 7, 8, 25, 24]
+leds = leds[::-1]
 comp = 4
 troyka = 17
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(dac, GPIO.OUT)
-GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(dac + leds, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT)
 GPIO.setup(comp, GPIO.IN)
 
+v = []
+
 try:
-    while True:
+    t0 = time.time()
+    GPIO.output(troyka, GPIO.HIGH)
+    while voltage <= 0.97 * 3.3:
         value, signal = adc()
+        GPIO.output(leds, signal)
         voltage = round(value / 256 * 3.3, 2)
-        print(f"Цифровое значение: {value} -> {signal}, значение напряжения: {voltage}")
+        v.append(voltage)
+        # print(f"Цифровое значение: {signal} -> {value}, значение напряжения: {voltage}")
+    GPIO.output(troyka, GPIO.LOW)
+    while voltage >= 0.02 * 3.3:
+        value, signal = adc()
+        GPIO.output(leds, signal)
+        voltage = round(value / 256 * 3.3, 2)
+        v.append(voltage)
+        # print(f"Цифровое значение: {signal} -> {value}, значение напряжения: {voltage}")
+    t1 = time.time()
+    dt = t1 - t0
+    with open('data.txt', 'w') as f:
+        for value in v:
+            f.write(value)
 finally:
-    GPIO.output(dac + [troyka], GPIO.LOW)
+    GPIO.output(dac + [troyka] + leds, GPIO.LOW)
     GPIO.cleanup()
